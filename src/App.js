1f2519dd54ee834208ae3d0a8c6e210fe1e970d9
CodinMaster <latheyapoorv@gmail.com>
2021-08-26 16:51:22 +0530

    Add chakra ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Button, useColorMode, Flex, Heading, Spacer } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+
 function App() {
-  return <div>Hello World</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  const underlineColor = { light: "gray.500", dark: "gray.400" };
+
+  return (
+    <Flex
+      py="4"
+      px={["2", "4", "10", "10"]}
+      borderBottom="2px"
+      borderBottomColor={underlineColor[colorMode]}
+    >
+      <Spacer flex="1" />
+      <Heading maxW={["302px", "4xl", "4xl", "4xl"]}>Hello World</Heading>
+      <Flex flex="1" justifyContent="flex-end">
+        <Button onClick={toggleColorMode} rounded="full" h="40px" w="40px">
+          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ChakraProvider>,
+  document.getElementById("root")
 );
